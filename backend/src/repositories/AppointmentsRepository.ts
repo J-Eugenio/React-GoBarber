a4bdcab6f2ad97d314319ae6ef8a8affe2f9fc7b
J-Eugenio <eugeniopb4@gmail.com>
2020-08-13 19:46:29 -0300

    Repositorio do TypeORM

--- backend/src/repositories/AppointmentsRepository.ts
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -1,39 +1,14 @@
-import { isEqual } from 'date-fns'
 import Appointment from '../models/Appointment'
+import { EntityRepository, Repository } from 'typeorm'
 
-interface CreateAppointmentDTO {
-    provider: string;
-    date: Date;
-}
-
-class AppointmentsRepository {
-    //vetor que recebe os dados
-    private appointments: Appointment[]
-
-    constructor() {
-        //inicializa o vetor
-        this.appointments = []
-    }
-
-    //retorna todos os dados do vetor
-    public all(): Appointment[]{
-        return this.appointments
-    }
-
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment>{
     //procura por data iguais ja cadastradas
-    public findByDate(date: Date): Appointment | null {
-        const findAppointment = this.appointments.find(appointment =>
-            isEqual(date, appointment.date)
-        )
-
+    public async findByDate(date: Date): Promise<Appointment | null> {
+        const findAppointment = await this.findOne({
+            where: { date }
+        })
         return findAppointment || null
-    }
-
-    public create({provider, date}: CreateAppointmentDTO): Appointment {
-        const appointment = new Appointment({provider, date})
-        this.appointments.push(appointment)
-
-        return appointment
     }
 }
 
