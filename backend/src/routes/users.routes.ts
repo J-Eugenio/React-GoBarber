ce118efcf80f5f339f20400df11ae3e945f76a41
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-08-30 20:08:49 -0300

    Atualizando avatar

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import multer from 'multer'
 import uploadConfig from '../config/upload'
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
@@ -23,9 +24,9 @@ usersRouter.post('/', async (request, response) => {
         delete user.password
 
         return response.json(user)
-    } catch (error) {
+    } catch (err) {
         return response.status(400).json({
-            error: error.message
+            error: err.message
         })
     }
 })
@@ -35,7 +36,24 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        return response.json({ ok: true })
+        try {
+
+            const updateUserAvatar = new UpdateUserAvatarService()
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename
+            })
+
+            delete user.password
+            
+            return response.json(user)
+
+        } catch (err) {
+            return response.status(400).json({
+                error: err.message
+            })
+        }
     }
 )
 
