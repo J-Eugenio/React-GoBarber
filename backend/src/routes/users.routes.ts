d3e4e7c841eafd4913cbdce975aed44f7b2653d0
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-08-30 19:39:20 -0300

    Upload de arquivos

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
-
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 import CreateUserService from '../services/CreateUserService'
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -17,12 +21,22 @@ usersRouter.post('/', async (request, response) => {
         })
 
         delete user.password
-        
+
         return response.json(user)
     } catch (error) {
-        return response.status(400).json({ 
-            error: error.message 
+        return response.status(400).json({
+            error: error.message
         })
     }
 })
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        return response.json({ ok: true })
+    }
+)
+
 export default usersRouter
