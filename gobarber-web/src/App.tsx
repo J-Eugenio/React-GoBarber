0cfec4a5d8ad91cac46af1a2ba7972f2a76ee398
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-09-16 21:48:16 -0300

    Contexto, login por context

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-//import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+//import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
     <>
-        <SignUp/>
+        <AuthProvider>
+            <SignIn />
+        </AuthProvider>
         <GlobalStyle />
     </>
 );
+
 
 export default App;
--- gobarber-web/src/pages/Signin/index.tsx
+++ gobarber-web/src/pages/Signin/index.tsx
@@ -1,35 +1,78 @@
-import React from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../Utils/getValidationErrors';
 
+import { AuthContext } from '../../context/AuthContext';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background, } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-    <Container>
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
+interface SignInFormData {
+    email: string;
+    password: string;
+}
 
-            <form>
-                <h1>Faça seu logon</h1>
+const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
 
-                <Input name="email" icon={FiMail} placeholder="E-Mail" />
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+    const { signIn } = useContext(AuthContext);
 
-                <Button type="submit">Entrar</Button>
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
+        try {
+            formRef.current?.setErrors({});
+            const schema = Yup.object().shape({
+                email: Yup.string().required('E-Mail obrigatório').email('Digite um e-mail válido'),
+                password: Yup.string().required('Senha obrigatória'),
+            });
 
-                <a href="forgot">Esqueci minha senha</a>
-            </form>
+            await schema.validate(data, {
+                abortEarly: false,
+            });
 
-            <a href="login">
-                <FiLogIn />
+            signIn({
+                email: data.email,
+                password: data.password,
+            });
+        } catch (err) {
+
+            const errors = getValidationErrors(err);
+
+            formRef.current?.setErrors(errors);
+        }
+    }, [signIn]);
+
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
+
+                <Form
+                    onSubmit={handleSubmit}
+                    ref={formRef}
+                >
+                    <h1>Faça seu logon</h1>
+
+                    <Input name="email" icon={FiMail} placeholder="E-Mail" />
+                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+
+                    <Button type="submit">Entrar</Button>
+
+                    <a href="forgot">Esqueci minha senha</a>
+                </Form>
+
+                <a href="login">
+                    <FiLogIn />
                 Criar Conta
             </a>
-        </Content>
-        <Background />
-    </Container>
-);
+            </Content>
+            <Background />
+        </Container>
+    );
+};
 
 export default SignIn;
