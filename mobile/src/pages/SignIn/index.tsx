ba59fad971cc52f41be2341b2cdd195e56015ea6
J-Eugenio <eugeniopb4@gmail.com>
2020-11-23 20:20:11 -0300

    Autenticação de usuário

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import { useAuth } from '../../hooks/auth';
+
 import getValidationErrors from '../../Utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -38,8 +40,12 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
+
   const navigation = useNavigation();
 
+  const { signIn, user } = useAuth();
+
+  console.log(user);
   const handleSignIn = useCallback(async (data: SignInFormData) => {
     try {
         formRef.current?.setErrors({});
@@ -50,6 +56,11 @@ const SignIn: React.FC = () => {
 
         await schema.validate(data, {
           abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
         });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
