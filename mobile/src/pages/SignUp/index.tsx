75be736961f59d9dcb8e88a6bb22310dabc2b495
J-Eugenio <eugeniopb4@gmail.com>
2020-11-23 19:22:05 -0300

    Cadastro de usuário

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -13,6 +13,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../Utils/getValidationErrors';
 import Input from '../../components/Input';
@@ -46,25 +47,34 @@ const SignUp: React.FC = () => {
         const schema = Yup.object().shape({
           name: Yup.string().required('Nome obrigatório'),
           email: Yup.string().required('E-Mail obrigatório').email('Digite um e-mail válido'),
-          password: Yup.string().required('Senha obrigatória').min(6, 'Mínimo de 6 digitos.'),
+          password: Yup.string().min(6, 'Mínimo de 6 digitos.'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
+
+        await api.post('/users', data);
+
+        Alert.alert(
+          'Cadastrado realizado com sucesso!',
+          'Você jâ pode fazer login na aplicação.',
+        );
+        navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
         formRef.current?.setErrors(errors);
+
         return;
       }
 
       Alert.alert(
-        'Erro na autenticação',
+        'Erro no cadastro',
         'Ocorreu um erro ao fazer o cadastro.',
       );
     }
-  }, []);
+  }, [navigation]);
 
   return (
     <>
