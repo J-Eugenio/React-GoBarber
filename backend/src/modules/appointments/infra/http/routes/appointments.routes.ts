ddfff62cf27b56a7449a0cecb5c072984d3d0cd1
eug <eugeniopb4@gmail.com>
2021-05-07 16:57:54 -0300

    criacao dos controllers - a terminar

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-import CreateAppointmentService from '@module/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@module/users/infra/html/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
-
-// appointmentsRouter.use(ensureAuthenticated);
+const appointmentsController = new AppointmentsController()
+appointmentsRouter.use(ensureAuthenticated);
 
 /* appointmentsRouter.get('/', async (request, response) => {
   const appointments = await appointmentsRepository.find();
@@ -16,17 +14,6 @@ const appointmentsRouter = Router();
     .json(appointments);
 }); */
 
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
+appointmentsRouter.post('/', appointmentsController.create);
 
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService)
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-  return response.json(appointment);
-});
 export default appointmentsRouter;
