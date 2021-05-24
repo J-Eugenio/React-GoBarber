f73119ce3e2855de80aba84a565b44e09719550f
eug <eugeniopb4@gmail.com>
2021-05-24 09:44:02 -0300

    Coverage report, testes de agendamento

--- backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,28 +1,28 @@
-import { getRepository, Repository } from 'typeorm';
 import Appointment from '@module/appointments/infra/typeorm/entites/Appointment';
+import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@module/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@module/appointments/dtos/ICreateAppointmentDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository {
-  private ormRepository: Repository<Appointment>
-
-  constructor() {
-    this.ormRepository = getRepository(Appointment)
-  }
+  private appointments: Array<Appointment> = [];
 
   // procura por data iguais ja cadastradas
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.ormRepository.findOne({
-      where: { date },
-    });
-    return findAppointment;
+    const findAppointment = this.appointments.find((appointment) => isEqual(appointment.date, date))
+
+    return findAppointment
   }
 
   public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
-    const appointment = this.ormRepository.create({ provider_id, date });
-    await this.ormRepository.save(appointment)
-    return appointment
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id })
+
+    this.appointments.push(appointment)
+
+    return appointment;
   }
 }
 
